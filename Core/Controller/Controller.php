1a09a42ae8c16d2d26d8a40f6b4f57a350d829e6
Unknown <arthur.simonian@outlook.fr>
2018-04-06 21:25:10 +0200

    utiliser redircto

--- Core/Controller/Controller.php
+++ Core/Controller/Controller.php
@@ -1,27 +1,27 @@
 <?php
-	namespace Core\Controller;
 
-	abstract class Controller{
+namespace Core\Controller;
 
-		protected $viewPath;
-		protected $template;
+abstract class Controller {
 
-		protected function render($view, $variables = []){
-			ob_start();
-			extract($variables);
-			require ($this->viewPath = '../app/views/' . str_replace('.', '/', $view) . '.php');
-			$content = ob_get_clean();
-			require ( '../app/views/templates/' . $this->template . '.php');
-		}
+    protected $viewPath;
+    protected $template;
 
-		protected function forbidden(){
-			header('location: index.php?p=users.login');
-		}
+    protected function render($view, $variables = []) {
+        ob_start();
+        extract($variables);
+        require ($this->viewPath = '../app/views/' . str_replace('.', '/', $view) . '.php');
+        $content = ob_get_clean();
+        require ( '../app/views/templates/' . $this->template . '.php');
+    }
 
-	protected function redirectTo($url){
-		header('Location: ' . $url);
+    protected function forbidden() {
+        $this->redirectTo('index.php?p=users.login');
+    }
+
+    protected function redirectTo($url) {
+        header('Location: ' . $url);
         exit();
-	}
+    }
 
 }
-?>
